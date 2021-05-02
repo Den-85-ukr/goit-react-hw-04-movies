6f72590ab3fe3dc0e06e674763184d1aa0811cce
Den-85-ukr <madden85@ukr.net>
2021-05-02 11:43:39 +0300

    cleaning the project from garbage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 //const apiKey = "ccf904bfa0cd14268f5b456c99ece816";
@@ -7,18 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
